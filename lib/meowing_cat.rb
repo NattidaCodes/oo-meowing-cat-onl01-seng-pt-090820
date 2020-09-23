e473a4aa3d34697f2ea944e7f1b3e95e7f023929
Nattida Wong <nattidac.wong@gmail.com>
2020-09-23 19:54:29 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -3,19 +3,8 @@ class Cat
   attr_accessor :name
 end
 
-class Sound
-  attr_accessor :meow
   def meow
-    puts "it is able to #{sound}!"
+    puts "meow!"
   end
 
 end
-
-maru = Cat.new
-maru.name = "Maru"
-
-sound = Sound.new
-sound.meow = "meow!"
-
-maru.name
-sound.meow
